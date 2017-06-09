08dc4c32d078d8922b18e86b7da7e2f754285e5b
gaelinilog <gaelinilog@WorkStation>
2017-06-09 12:02:09 +0200

    add basic action

--- src/model/actions.py
+++ src/model/actions.py
@@ -8,3 +8,26 @@ qualités.
 
 .. moduleauthor:: Gaël PICOT <gael.picot@free.fr>
 '''
+import math
+import dice
+
+
+class ResultatAction(object):
+    """ resultat d'une action
+    """
+    def __init__(self, pc_reussit):
+        """ init
+        """
+        self._lancer = dice.roll("1d100+0")
+        self._reusite = pc_reussit <= self._lancer
+
+
+def action(self, competance=10, ajustement=0):
+    """ jouer une action
+    """
+    pc_reussit = 0
+    if ajustement >= -8:
+        pc_reussit = math.floor(competance * ((ajustement + 10) / 2))
+    elif ajustement >= -10:
+        pc_reussit = math.floor(competance / (ajustement * 2))
+    return ResultatAction(pc_reussit)
