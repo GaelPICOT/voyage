4ba3810093c8d633a0e57d4910141b0f15de84ac
inilog <inilog@tour>
2017-07-20 07:52:10 +0200

    ajout base haut-rêve

--- src/model/rules/rdd/hautreve.py
+++ src/model/rules/rdd/hautreve.py
@@ -7,3 +7,56 @@
 
 .. moduleauthor:: Gaël PICOT <gael.picot@free.fr>
 '''
+from enum import Enum
+
+
+class CaseTMR(object):
+    """ représente une case en TMR
+    """
+    class Categorie(Enum):
+        cite = 0
+        coline = 1
+        desert = 2
+        desolation = 3
+        foret = 4
+        gouffre = 5
+        monts = 6
+        necropole = 7
+        plaine = 8
+        pont = 9
+        sanctuaire = 10
+        fleuve = 11
+        lac = 12
+        marais = 13
+
+    def __init__(self):
+        """ init
+        """
+
+
+class TMR(object):
+    """ Represente les TMR pour une personne
+    """
+
+
+class Sort(object):
+    """ représente un sort
+    """
+    def __init__(self):
+        """ init
+        """
+
+
+class Rituel(Sort):
+    """ représente un rituel
+    """
+
+
+class HautReve(object):
+    """ gestion du haut-rêve
+    """
+    def __init__(self):
+        """ init
+        """
+        # point de sort
+        self._pt_sort = {"Oniros": 0, "Hypnos": 0, "Narcos": 0, "Thanatos": 0}
--- src/model/rules/rdd/sante.py
+++ src/model/rules/rdd/sante.py
@@ -8,6 +8,8 @@
 .. moduleauthor:: Gaël PICOT <gael.picot@free.fr>
 '''
 from enum import Enum
+from model.rules.rdd import voyageur
+import dice
 
 
 class PremierSoin(object):
@@ -20,18 +22,38 @@ class SoinComplet(object):
     """
 
 
+class encaissement(object):
+    """ représente un jet d'encaissement pour un personnage
+    """
+    class Table(Enum):
+        """ table coup non-mortel ou blessures
+        """
+        nonmortel = 0
+        blessure = 1
+
+    def __init__(self, personnage: voyageur, lancer: int=None, pdom: int=0,
+                 table: encaissement.Table=encaissement.Table.blessure):
+        """ init
+        """
+        if lancer is None:
+            lancer = dice.roll("2d10+0")
+        self._personnage = personnage
+        self._lancer = lancer + pdom
+        self._pdom = pdom
+        self._table = table
+
+
 class Blessure(object):
     """ decris une blessure
     """
     class TypeB(Enum):
         """ diferant type de blessure
         """
-        eraflure = 0
         legere = 2
         grave = 4
         critique = 6
 
-    def __init__(self, typeB=Blessure.TypeB.eraflure):
+    def __init__(self, typeB: Blessure.TypeB=Blessure.TypeB.eraflure):
         """ init
         """
         self._type = typeB
