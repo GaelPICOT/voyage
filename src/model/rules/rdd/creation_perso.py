58092c9ca960e8a37c2d78b458d52dac2a4e1fb7
paradoxisme <gael.picot@free.fr>
2017-07-30 20:12:45 +0200

    amélioration création personnage

--- src/model/rules/rdd/creation_perso.py
+++ src/model/rules/rdd/creation_perso.py
@@ -7,3 +7,22 @@
 
 .. moduleauthor:: Gaël PICOT <gael.picot@free.fr>
 '''
+from model.rules.rdd.voyageur import Personnage
+
+
+class PersonnageCreateur(object):
+    """ créateur de personnage
+    """
+    def __init__(self):
+        """ init
+        """
+        self._personnage_courrant = Personnage()
+        self._point_carac = 20
+        self._point_comp = 3000
+        self._point_sort = 0
+
+    @property
+    def personnage(self):
+        """ renvoi le personnage
+        """
+        return self._personnage_courrant
--- src/view/personnage.py
+++ src/view/personnage.py
@@ -10,15 +10,28 @@
 from PyQt5.QtWidgets import QWidget
 from PyQt5.uic import loadUi
 import os
+from model.rules.rdd.creation_perso import PersonnageCreateur
 
 
 class PersonnageWindow(QWidget):
     """ fenetre principale
     """
-    def __init__(self, parent=None, mdi_area=None):
+    def __init__(self, parent=None, mdi_area=None, personnage=None,
+                 perso_creat=None):
         """ init
         """
+        if personnage is None:
+            if perso_creat is None:
+                self._personnage_createur = PersonnageCreateur()
+            else:
+                self._personnage_createur = perso_creat
         QWidget.__init__(self, parent)
         current_rep = os.path.abspath(os.path.split(__file__)[0])
         loadUi(os.path.join(current_rep, "personnage.ui"), self)
         self._mdi_area = mdi_area
+        self.revant_button.clicked.connect(self.change_reve_statu)
+
+    def change_reve_statu(self):
+        """ change haut revant en vrai revant et inversement
+        """
+        self.revant_button.setStyleSheet("QPushButton {color : blue; }")
